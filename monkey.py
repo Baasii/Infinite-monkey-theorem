d54c210b33afdd49071721407dd6e6d4d67d6de8
Lauri Mäntylä <lauri.ma@hotmail.com>
2022-10-03 10:04:44 +0300

    Cleaned unused code

--- monkey.py
+++ monkey.py
@@ -1,4 +1,3 @@
-from operator import truediv
 import string
 import random
 import time
@@ -10,52 +9,41 @@ with open('hamlet.txt') as hamlet:
             lower.write(y) #Read hamlet and change it into lowercase
 
     with open('hamletLower.txt', "r") as lower: #Open in read mode
-        with open('output.txt', 'r+') as output:
-            #put = output.read()
-            
-            f = (lower.read().split()) #Read unique words and split
-            
+     
+        f = (lower.read().split()) #Read unique words and split
+                  
+        longest_word =  max(f, key=len) #Read longest word and its length
+        shortest_word = min(f, key=len) #Read shortest word and its length
 
-                       
-            longest_word =  max(f, key=len) #Read longest word and its length
-            shortest_word = min(f, key=len) #Read shortest word and its length
-
-            longestLength = len(longest_word)
-            shortestLength = len(shortest_word)
+        longestLength = len(longest_word)
+        shortestLength = len(shortest_word)
+        
+        
+        with open("foundWords.txt", "a+") as correctWords:
+            print('Starting comparison')
             
-            
-            with open('foundWords.txt', 'a+') as correctWords:
-                print('Starting comparison')
-                letters = string.ascii_lowercase + string.punctuation
-                loop = True
-                foundCount = 0
-                currentWord=0
-                while(loop):
-                    inputLength = random.randint(shortestLength, longestLength) #Generate random length between shortest and longest word
-                    typedWord = ( ''.join(random.choice(letters) for i in range(inputLength)) ) #Random string
-                    print(typedWord)
-                    time.sleep(0.00011) #Time between comparisons
-                    typedWord = "francisco"
+            letters = string.ascii_lowercase + string.punctuation
+            loop = True
+            foundCount = 0
+            currentWord=0
+            while(loop):
+                inputLength = random.randint(shortestLength, longestLength) #Generate random length between shortest and longest word
+                typedWord = ( ''.join(random.choice(letters) for i in range(inputLength)) ) #Random string
+                print(typedWord)
+                time.sleep(0.00011) #Time between comparisons
+                typedWord = "francisco"
 
-                    
-                    if typedWord == f[currentWord]: #If string is found in Hamlet ->      
-                        currentWord +=1
-                        correctWords.write(typedWord + ' ') #Write found word into correctWords
-                        print('FOUND !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
-                        print('Word found: ' + typedWord)
-                        foundCount+=1 #How many words found
-                        print(foundCount, ' words found')
-                        
-
-                        time.sleep(2.2)
+                
+                if typedWord == f[currentWord]: #If string is found in Hamlet ->      
+                    currentWord +=1
+                    correctWords.write(typedWord + " ") #Write found word into correctWords
+                    print('Correct word !!!')
+                    print('Word found: ' + typedWord)
+                    foundCount+=1 #How many words found
+                    print(foundCount, ' words found')
                     
 
-                    ###### TODO ######
-                    # Fix write found words
-                    # Subprocess
-                    
-
-
-
-
-
+                    time.sleep(2.2)
+                
+                ###### TODO ######
+                # Subprocess, multiple monkeys at same time
